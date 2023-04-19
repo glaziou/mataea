6e8e352d6d03a9a1fc2d4a58da2a360b2cd54921
Philippe GLAZIOU <glaziou@gmail.com>
2023-04-19 10:46:39 -1000

    updated weights

--- R/01-init.R
+++ R/01-init.R
@@ -33,11 +33,31 @@ library(haven)
 #
 mata <- data.table(read_dta(here('stata/bases/Stata/02_MATAEA_final.dta')))
 geo <- data.table(read_dta(here('stata/bases/Stata/Mataea Liste ID Commune.dta')))
+pop <- data.table(read_dta(here('stata/bases/Stata/ponderation_draft.dta')))
 
 # check for dups
 sum(duplicated(mata$subjid))==0
 
+# data prep
+mata$geo <- haven::as_factor(mata$archipelago)
+mata$agegr <- haven::as_factor(mata$age_cat)
+mata$sex <- haven::as_factor(mata$gender)
+
+# include w1 weights
+out1 <- mata[, .N, by=.(geo,age_cat,gender)]
+out2 <- merge(out1, pop[, .(geo=archipelago,gender,age_cat,pop=N)], 
+              by=c("geo", "age_cat","gender"))
+out2[, w1 := pop/N]
+tmp <-
+  merge(mata,
+        out2,
+        by = c('geo', 'age_cat','gender'),
+        all.x = TRUE)
+dim(mata); dim(tmp)
+mata <- copy(tmp)
+
 # save R binary
 save(mata, file = here('data/mata.rdata'))
 save(geo, file = here('data/geo.rdata'))
+save(pop, file = here('data/pop.rdata'))
 
