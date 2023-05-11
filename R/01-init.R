3d82d52fea0894da35d538fa4305335f55fe21f9
Philippe GLAZIOU <glaziou@gmail.com>
2023-05-10 16:04:36 -1000

    updated weights separating Gambier from Tuamotu

--- R/01-init.R
+++ R/01-init.R
@@ -20,6 +20,7 @@
 library(data.table)
 library(here)
 library(haven)
+library(readxl)
 
 # load curated datasets in stata proprietary format
 #
@@ -33,25 +34,33 @@ library(haven)
 #
 mata <- data.table(read_dta(here('stata/bases/Stata/02_MATAEA_final.dta')))
 geo <- data.table(read_dta(here('stata/bases/Stata/Mataea Liste ID Commune.dta')))
-pop <- data.table(read_dta(here('stata/bases/Stata/ponderation_draft.dta')))
+# pop <- data.table(read_dta(here('stata/bases/Stata/ponderation_draft.dta')))
+pop <- data.table(read_excel(here('input/ponderation.xlsx'), sheet=1))
 
 # check for dups
 sum(duplicated(mata$subjid))==0
 
 # data prep (strata)
 mata$geo <- haven::as_factor(mata$archipelago)
+mata$ile <- haven::as_factor(mata$island)
+mata[ile == "Mangareva", geo := 'Gambier']
+mata[geo == "Tuamotu-Gambier", geo := 'Tuamotu']
+mata$geo <- droplevels(mata$geo)
+
 mata$agegr <- haven::as_factor(mata$age_cat)
 mata$sex <- haven::as_factor(mata$gender)
+pop[, sex := "Male"]
+pop[gender_label=="femmes", sex := "Female"]
 
 # w1 weights
-out1 <- mata[, .N, by=.(geo,age_cat,gender)]
-out2 <- merge(out1, pop[, .(geo=archipelago,gender,age_cat,pop=N)], 
-              by=c("geo", "age_cat","gender"))
+out1 <- mata[, .N, by=.(geo,age_cat,sex)]
+out2 <- merge(out1, pop[, .(geo=archipelago,sex,age_cat,pop)], 
+              by=c("geo", "age_cat","sex"))
 out2[, w1 := pop/N]
 tmp <-
   merge(mata,
         out2,
-        by = c('geo', 'age_cat','gender'),
+        by = c('geo', 'age_cat','sex'),
         all.x = TRUE)
 dim(mata); dim(tmp)
 mata <- copy(tmp)
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Mata'ea research programme in French Polynesia
 - data: latest version datasets in R binary format
 - doc: documentation including survey questionnaire
 - stata: raw data, processed datasets in stata format
+- input: input data
 - html: report pages
 - R: R and R markdown scripts
 
