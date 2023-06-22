6729ee4572ba86ce42bfb522a5fc2b6a7e7af8c5
Philippe GLAZIOU <glaziou@gmail.com>
2023-06-22 09:03:48 -1000

    updated VHB/fib4

--- R/02-dataprep.R
+++ R/02-dataprep.R
@@ -229,8 +229,8 @@ mata[, civil := as_factor(civil_state4)]
 mata[civil %in% c("Missing data"), civil := NA]
 mata$civil <- droplevels(mata$civil)
 
-mata[fib_4_cat2 != 99, fib4 := droplevels(as_factor(fib_4_cat2))]
-levels(mata$fib4) <- c("Cat 1-2", "Cat 1-2", "Cat 3")
+mata[fib_4_cat != 99 & !is.na(vhb), fib4 := droplevels(as_factor(fib_4_cat))]
+levels(mata$fib4) <- c("Normal", "Normal", "Parcours de soin")
 
 mata[diabete != 99, diabetes := droplevels(as_factor(diabete))]
 levels(mata$diabetes) <- c("Non-diabetic", "Non-diabetic", "Type-2 diabetes")
